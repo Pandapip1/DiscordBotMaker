ac83f8923fe035215eb9fdde25b6b6ecc1097ec9
actions-user <actions-user@github.com>
2021-09-30 17:51:26 +0000

    Auto-push

---
+++ monacoworker.js
@@ -0,0 +1,5 @@
+// see https://github.com/Microsoft/monaco-editor
+self.MonacoEnvironment = { 
+    baseUrl: "/DiscordBotMaker/" 
+};
+importScripts("/DiscordBotMaker/vs/base/worker/workerMain.js");
